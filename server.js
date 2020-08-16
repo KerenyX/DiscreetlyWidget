d593d6192f1f95e6943e64a36e2f46784e169266
KerenyX <kereny82@gmail.com>
2020-08-16 16:24:30 +0300

    nothing

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const outChatController = require('./controllers/outChatController.js');
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true})); 
 app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
