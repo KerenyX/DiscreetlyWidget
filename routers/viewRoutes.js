d54ec3566e67a978689600492dd083446c8f410c
KerenyX <kereny82@gmail.com>
2020-08-16 15:01:35 +0300

    Html new folder

--- routers/viewRoutes.js
+++ routers/viewRoutes.js
@@ -2,13 +2,10 @@ const express = require("express");
 const viewRouter = express.Router();
 const path = require('path');
 
-const parentPath = path.join(__dirname, '../', '/public/');
+const parentPath = path.join(__dirname, '../', '/public/html/');
 
 viewRouter.get('/', (req, res) => {
     res.sendFile(parentPath + `${process.env.ENTRY_HTML}.html`);
-});
-viewRouter.get('/kolmila', (req, res) => {
-    res.sendFile(parentPath + 'kolmila.html');
 });
 viewRouter.get('/discreetly', (req, res) => {
     res.sendFile(parentPath + 'discreetly.html');
