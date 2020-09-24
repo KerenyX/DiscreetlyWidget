a3dd8ed855e4d344920afe0cedfcaaf76386efcd
KerenyX <kereny82@gmail.com>
2020-09-24 19:32:28 +0300

    tokens in English

---
+++ public/langs/en.js
@@ -0,0 +1,16 @@
+const lang = {
+    "clickToCloseChat": "click to close chat",
+    "closingChatEraseMessages": "closing chat erase messages",
+    "connecting": "connecting...",
+    "conversationEnded": "Conversation ended",
+    "endConversation": "End conversation",
+    "helloImVolFromTheHelpCenterYouCanStartTyping": "Hello, I'm volunteer from the help center. You can start typing",
+    "me": "Me",
+    "muteNotifications": "Mute notifications",
+    "oneOfOurVolsWillBeWithYouShortly": "One of our volunteers will be with you shortly",
+    "reopenChatStartsWithNewVol": "Reopen chat starts with new volunteer",
+    "turnOnNotifications": "Turn on notifications",
+    "typeTextHere": "Type text here",
+    "vol": "Volunteer",
+    "volIsTyping": "Volunteer is typing..."
+}
