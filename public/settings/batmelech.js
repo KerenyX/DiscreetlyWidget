b6a9e77a9dd34fb25efcbfcb54d99490bc7e5a68
KerenyX <kereny82@gmail.com>
2020-10-04 14:55:00 +0300

    temp for testing

--- public/settings/batmelech.js
+++ public/settings/batmelech.js
@@ -2,7 +2,7 @@ const settings = {
     notificationDefault: false,
     timeLimit: true,
     startTime: 9,
-    endTime: 14,
+    endTime: 22,
     days: [1, 1, 1, 1, 1, 0, 0],
     notAvailableMessage: "שירות פעיל רק בימים א-ה בשעות 9:00-14:00"
 }
