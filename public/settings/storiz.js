098c0a728fcaebd36a01e431d34ac8589415e48c
AssafHizki <62558803+AssafHizki@users.noreply.github.com>
2020-10-19 10:46:40 +0300

    Create storiz.js

---
+++ public/settings/storiz.js
@@ -0,0 +1,10 @@
+const settings = {
+    notificationDefault: true,
+    timeLimit: true,
+    startTime: 10,
+    endTime: 14,
+    days: [1, 1, 1, 1, 1, 1, 1],
+    notAvailableMessage: "תודה שפניתם אלינו, שעות הפעילות שלהצ'אט הינן 16:00-22:00",
+    autoHelloFromVol: true,
+    autoMinimizeOnBlur: true
+}
